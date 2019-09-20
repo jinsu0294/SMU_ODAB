767c2150a899fa71bdb7204c26a07869cc52db89
jinsu0294 <jinsususu@gmail.com>
2019-09-20 18:17:58 +0900

    fix solve quiz

--- app/src/main/java/com/example/smu_quiz_2/QuizSolveActivity.kt
+++ app/src/main/java/com/example/smu_quiz_2/QuizSolveActivity.kt
@@ -1,12 +1,12 @@
 package com.example.smu_quiz_2
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.Toast
 import com.example.smu_quiz_2.data_class.Quiz
-import com.example.smu_quiz_2.data_class.QuizList
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.schedulers.Schedulers
 import kotlinx.android.synthetic.main.activity_quiz_solve.*
@@ -15,82 +15,65 @@ class QuizSolveActivity : AppCompatActivity() {
 
     var smuOdabAPI = SmuOdabAPI()
     var smuInfoRetrofit = smuOdabAPI.smuInfoRetrofit()
-    var smuOdabInterface = smuInfoRetrofit.create(SmuOdabInterface::class.java)
-
+    var smuOdabInterface: SmuOdabInterface = smuInfoRetrofit.create(SmuOdabInterface::class.java)
     var quiz = arrayListOf<Quiz>()
-    var quizlist = arrayListOf<QuizList>()
-    var i =0
+    var index = 0
+    @SuppressLint("CheckResult")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_quiz_solve)
 
-        smuOdabInterface.getQuiz(quizlist[i].quiz_id.toString())
+        val quizId = intent.getIntegerArrayListExtra("quizId")
+
+        Log.e("23423400", quizId.toString())
+        smuOdabInterface.getSelectQuizList(quizId)
             .subscribeOn(Schedulers.io())
             .observeOn(AndroidSchedulers.mainThread())
-            .subscribe({ quiz ->
-                Log.e("jinsu", quiz[i].title)
-                tvUserQuizTitle.text = quiz[i].title
-                tvUserQuizContents.text = quiz[i].text
-                tvUserQuiz_1.text = quiz[i].choice_1
-                tvUserQuiz_2.text = quiz[i].choice_2
-                tvUserQuiz_3.text = quiz[i].choice_3
-                tvUserQuiz_4.text = quiz[i].choice_4
-                tvUserQuizExplanation.text = quiz[i].explain
+            .subscribe({ list ->
+                for (lists in list) {
+                    quiz.add(lists)
+                }
+                Log.e("23423400", quiz.toString())
+
+                setQuiz(index)
             }, { error ->
                 error.printStackTrace()
             }, {
             })
 
-        tvUserQuizExplanation.visibility= View.INVISIBLE
+
+        tvUserQuizExplanation.visibility = View.INVISIBLE
         tvExplanation.setOnClickListener {
-            tvUserQuizExplanation.visibility= View.VISIBLE
+            tvUserQuizExplanation.visibility = View.VISIBLE
         }
 
         btnPrev.setOnClickListener {
-            if(i-1<0){
-                Toast.makeText(this,"이전 문제가 없습니다.",Toast.LENGTH_SHORT).show()
-            }else{
-                smuOdabInterface.getQuiz(quizlist[i-1].quiz_id.toString())
-                    .subscribeOn(Schedulers.io())
-                    .observeOn(AndroidSchedulers.mainThread())
-                    .subscribe({ quiz ->
-                        Log.e("jinsu", quiz[i-1].title)
-                        tvUserQuizTitle.text = quiz[i-1].title
-                        tvUserQuizContents.text = quiz[i-1].text
-                        tvUserQuiz_1.text = quiz[i-1].choice_1
-                        tvUserQuiz_2.text = quiz[i-1].choice_2
-                        tvUserQuiz_3.text = quiz[i-1].choice_3
-                        tvUserQuiz_4.text = quiz[i-1].choice_4
-                        tvUserQuizExplanation.text = quiz[i-1].explain
-                    }, { error ->
-                        error.printStackTrace()
-                    }, {
-                    })
+            if (index-1 < 0) {
+                Toast.makeText(this, "이전 문제가 없습니다.", Toast.LENGTH_SHORT).show()
+            } else {
+                index--
+                setQuiz(index)
             }
         }
 
         btnNext.setOnClickListener {
-            if(i+1<quiz.size){
-                Toast.makeText(this,"다음 문제가 없습니다.",Toast.LENGTH_SHORT).show()
-            }else{
-                smuOdabInterface.getQuiz(quizlist[i+1].quiz_id.toString())
-                    .subscribeOn(Schedulers.io())
-                    .observeOn(AndroidSchedulers.mainThread())
-                    .subscribe({ quiz ->
-                        Log.e("jinsu", quiz[i+1].title)
-                        tvUserQuizTitle.text = quiz[i+1].title
-                        tvUserQuizContents.text = quiz[i+1].text
-                        tvUserQuiz_1.text = quiz[i+1].choice_1
-                        tvUserQuiz_2.text = quiz[i+1].choice_2
-                        tvUserQuiz_3.text = quiz[i+1].choice_3
-                        tvUserQuiz_4.text = quiz[i+1].choice_4
-                        tvUserQuizExplanation.text = quiz[i+1].explain
-                    }, { error ->
-                        error.printStackTrace()
-                    }, {
-                    })
+            if (index+1  > quiz.size) {
+                Toast.makeText(this, "다음 문제가 없습니다.", Toast.LENGTH_SHORT).show()
+            } else {
+                index++
+                setQuiz(index)
             }
         }
 
+    }
+
+    fun setQuiz(index: Int) {
+        tvUserQuizTitle.text = quiz[index].title
+        tvUserQuizContents.text = quiz[index].text
+        tvUserQuiz_1.text = quiz[index].choice_1
+        tvUserQuiz_2.text = quiz[index].choice_2
+        tvUserQuiz_3.text = quiz[index].choice_3
+        tvUserQuiz_4.text = quiz[index].choice_4
+        tvUserQuizExplanation.text = quiz[index].explain
     }
 }
